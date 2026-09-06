675966e612fad555558cccc0aca6a35314315d2f
Narayan Singh <samratsing04@gmail.com>
2026-09-07 01:14:26 +0530

    Added a file

---
+++ Array/inputAge.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class inputAge {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Your age is: ");
+        int age=sc.nextInt();
+        System.out.println("Your age is :"+age);
+    }
+}
